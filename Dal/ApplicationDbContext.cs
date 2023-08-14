0ebb66a8a03406c8c2ef32702736565aae0f702e
sirblck13 <70873411+sirblck13@users.noreply.github.com>
2023-08-13 23:14:33 -0400

    cleanup

--- Dal/ApplicationDbContext.cs
+++ Dal/ApplicationDbContext.cs
@@ -26,29 +26,5 @@ namespace Dal
         }
 
         #endregion
-
-        #region Overrides
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            var connString = "DefaultConnection";
-
-            if (!string.IsNullOrEmpty(connString))
-                optionsBuilder.UseMySql(connString, ServerVersion.AutoDetect(connString), opts =>
-                {
-                    opts.MigrationsAssembly("Dal");
-                    opts.CommandTimeout((int)TimeSpan.FromHours(1).TotalSeconds);
-
-                    opts.EnableRetryOnFailure(
-                        maxRetryCount: 5,
-                        maxRetryDelay: TimeSpan.FromSeconds(30),
-                        errorNumbersToAdd: null
-                        );
-                });
-
-            base.OnConfiguring(optionsBuilder);
-        }
-
-        #endregion
     }
 }
